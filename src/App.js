ccbfc178daf3783d1c3bc0ddefe77f3438b44f35
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-07-09 11:35:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import React, { useState } from 'react';
-import UsersList from './component/UsersList';
-import UserForm from './component/UserForm';
-import UserDetails from './component/UserDetails';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(null);
 
-  const addUser = (user) => {
-    setUsers([...users, user]);
-  };
-
-  const selectUser = (user) => {
-    setSelectedUser(user);
-  };
-
+function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Project Management System</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div>
-          <UserForm addUser={addUser} />
-        </div>
-        <div>
-          <UsersList users={users} selectUser={selectUser} />
-        </div>
-        <div>
-          {selectedUser && <UserDetails user={selectedUser} />}
-        </div>
-      </div>
+    <div className="App">
+     
     </div>
   );
-};
+}
 
 export default App;
